0e5243525ca3efd346da015ee0d3ff3be2cc3c4f
hishamalfatish <hisham.alfatish@gmail.com>
2025-09-07 12:43:47 +0200

    add mikrotik to ssh config

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -3,7 +3,6 @@
   lib,
   ...
 }: {
-  # ssh-agent
   services.ssh-agent.enable = true;
 
   programs.ssh = {
@@ -12,6 +11,15 @@
       atlas = lib.hm.dag.entryBefore ["*"] {
         hostname = "192.168.100.100";
         user = "root";
+        identitiesOnly = false;
+        extraOptions = {
+          PubkeyAuthentication = "no";
+          PreferredAuthentications = "password,keyboard-interactive";
+        };
+      };
+      mikrotik = lib.hm.dag.entryBefore ["*"] {
+        hostname = "192.168.88.1";
+        user = "admin";
         identitiesOnly = false;
         extraOptions = {
           PubkeyAuthentication = "no";
